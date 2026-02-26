ae63d63460322dfbf8a5b6c6626b285f75ea5dca
knamost <sumansenpai@gmail.com>
2026-02-26 22:10:56 +0545

    configued server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+import dotenv from "dotenv/config";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//? Routes
+app.get("/", (req, res) => {
+	res.send("Hello world");
+});
+
+//? server listening
+app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
