c47a880fe010c703e7837a274c9539a5fa850872
knamost <sumansenpai@gmail.com>
2026-02-27 23:06:02 +0545

    configued basic and cors configuration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-//? Middlewares
-app.use(express.json);
+//? basic configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+//? cors configuration
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173", // port: 5173 for vite
+		credentials: true,
+		methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	}),
+);
+
 //? Routes
 app.get("/", (req, res) => {
 	return res.send("Welcome to basecampy");
